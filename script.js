429dfbae21174bffe33b7da73b7e5d3d0620f1a2
oscramz <86433160+oscramz@users.noreply.github.com>
2021-09-14 16:58:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// Calculates operation values
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear() + parseInt(years);
     
+    var interest = (principal * years * rate) / 100; // Computes the interest
+    
+    // Validates the amount positve value
+        if (principal <= 0 || isNaN(principal)) {
+            alert("Enter a valid positive amount");
+            document.getElementById("principal").focus();
+            return
+        }
+    // Prints the results
+    document.getElementById("result").innerHTML="If you deposit: "+principal+"$,\<br\>at an interest rate of: "+rate+"%\<br\>You will receive an amount of: "+interest+"$,\<br\>in the year: "+year+"\<br\>"
 }
-        
+
+// Updates the rate of interest
+function updateRate (){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+}
